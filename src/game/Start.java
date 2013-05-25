ec7992d86b30aaee1ea9e43e8e9dcf28f9edd545
nemo9955 <nemo9955@gmail.com>
2013-05-25 10:48:46 +0300

    prima comitere

---
+++ src/game/Start.java
@@ -0,0 +1,17 @@
+package game;
+
+public class Start {
+
+	public Start() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
