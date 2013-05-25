909641e1ae8be24f744a1d44454c32059eff8dc3
nemo9955 <nemo9955@gmail.com>
2013-05-25 13:46:43 +0300

    patratul se misca

--- src/game/GamePlay/GameplayState.java
+++ src/game/GamePlay/GameplayState.java
@@ -2,6 +2,7 @@ package game.GamePlay;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -12,15 +13,28 @@ public class GameplayState extends BasicGameState {
 
     private float x = 100;
     private float y = 100;
+    
+    private Input input ;
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-
+        input=gc.getInput();
     }
 
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-
+        
+        if (input.isKeyDown(Input.KEY_W))
+            y -= 0.5f * delta;
+ 
+        if (input.isKeyDown(Input.KEY_S))
+            y += 0.5f * delta;
+ 
+        if (input.isKeyDown(Input.KEY_D))
+            x += 0.5f * delta;
+ 
+        if (input.isKeyDown(Input.KEY_A))
+            x -= 0.5f * delta;
     }
 
     @Override
